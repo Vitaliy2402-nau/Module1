48313aa16478d860b99f7ae7e9d9db7340bd0847
Vitaliy2402-nau <78072344+Vitaliy2402-nau@users.noreply.github.com>
2021-03-19 13:14:27 +0200

    Add files via upload

---
+++ Module1.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <math.h>
+#include <cmath>
+#define _USE_MATH_DEFINES
+using namespace std;
+/* variant ? 12, student of group 110, name Орел Віталій*/
+int main() {
+	 //Task №1, task text:Write an arithmetic expression in an algorithmic language and indicate the order of operations*/
+ float G;
+ double a,y,x;
+ cout<<"a="<<endl;
+ cout<<"y="<<endl;
+ cout<<"x="<<endl;
+{
+	G= pow(cos (abs(y+x) ), 3) - (x+y)/pow(atan(x+a),4) * pow(x, 5);
+}
+	return 0;
+}
+ //Task №2:Write logical expressions using conditional operators, relation operations, and logical
+ int main(){
+double k,l,n,m;
+if (k>0)
+{
+ {else	2*m>l;
+  k+m+l+n>0;
+}
+	else if (k<0);
+	 else n>3*l;
+	 k+m+l+n>0;
+	 
+}
+cout<<"Done!"<<endl;
+//Task №3
+int main(){
+	int result;
+float n,m;
+ (n , m<=1000);
+cout<<"n="<<endl;
+cout<<"m="<<endl;
+{
+result = pow(n , 2) - 2*a*b +pow(m, 2);
+cout<<"result"<<endl;
+}
+//Task №4
+int main(){
+	float A[12],sum,max,min;
+	int i;
+	for(i=0;i<n;i++)
+	min = A[11];
+	max = A[0];
+	cout<<"Enter value in element"<<i<<endl;
+	cin>>A[i];
+	sum = min+max
+	cout<<"Sum of arrays="<<sum<<endl;
+}
+	
+	
+}
